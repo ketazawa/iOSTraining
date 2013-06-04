063ffe8a3e509958b4d1587de596efb7f92aac9f
kenji.tazawa <kenji.tazawa@mixi.co.jp>
2013-06-04 14:53:58 +0900

    Add answer for HomeWork 2.2

--- HomeWorks/2.2/NavigationInModal/NavigationInModal/NIMViewController.m
+++ HomeWorks/2.2/NavigationInModal/NavigationInModal/NIMViewController.m
@@ -1,9 +1,9 @@
 //
-//  NIMViewController.m
-//  NavigationInModal
+//NIMViewController.m
+//NavigationInModal
 //
-//  Created by 武田 祐一 on 2013/04/17.
-//  Copyright (c) 2013年 武田 祐一. All rights reserved.
+//Created by 武田 祐一 on 2013/04/17.
+//Copyright (c) 2013年 武田 祐一. All rights reserved.
 //
 
 #import "NIMViewController.h"
@@ -18,20 +18,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.title = [NSString stringWithFormat:@"%d", self.navigationController.viewControllers.count];
+    
+    UIBarButtonItem *pushButton = [[UIBarButtonItem alloc]initWithTitle:@"Push" style:UIBarButtonItemStylePlain target:self action:@selector(pressPushButton)];
+    [self.navigationItem setRightBarButtonItem:pushButton];
+    
+    UIBarButtonItem *rootButton = [[UIBarButtonItem alloc]initWithTitle:@"Root" style:UIBarButtonItemStylePlain target:self action:@selector(pressRootButton)];
+    [self.navigationItem setLeftBarButtonItem:rootButton];
+    
+    self.navigationItem.leftItemsSupplementBackButton = YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //Dispose of any resources that can be recreated.
 }
-- (IBAction)modalButtonTapped:(id)sender {
 
-    // 一度モーダルで表示させたのには特に意味はありません(途中で宿題の中身を変えたためです)
-    NIMModalViewController *modal = [[NIMModalViewController alloc] initWithNibName:@"NIMModalViewController" bundle:nil];
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:modal];
-    [self presentViewController:navigationController animated:YES completion:nil];
+#pragma -mark PrivateMethod
+- (void)pressPushButton
+{
+    NSLog(@"Pressed Push Button");
+    
+    [self.navigationController pushViewController:[[NIMViewController alloc]init] animated:YES];
+}
+
+- (void)pressRootButton
+{
+    NSLog(@"Pressed Root Button");
+    [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 @end
